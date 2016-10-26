d74c1174e1f5d84936226cb405d6afe904ba1572
Dave Sims <davesims@github.com>
2016-10-26 09:51:18 -0500

    Added git install & git clone for kubos

--- vm/provision.sh
+++ vm/provision.sh
@@ -2,3 +2,7 @@ apt-get -y update
 apt-get -y install build-essential libssl-dev libffi-dev
 apt-get -y install python-setuptools python-dev
 apt-get -y install python-pip
+add-apt-repository ppa:git-core/ppa
+apt-get -y install git
+mkdir /home/vagrant/kubos
+git clone https://github.com/kubostech/kubos.git /home/vagrant/kubos
