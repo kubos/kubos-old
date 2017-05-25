c7e9cb69c6ab1877fc44f229b508c06049106434
Ryan Plauche <ryan@kubos.co>
2017-05-25 12:07:38 -0500

    Improving iobc target python script

--- targets/target-kubos-linux-isis-gcc/scripts/run.py
+++ targets/target-kubos-linux-isis-gcc/scripts/run.py
@@ -2,6 +2,11 @@
 
 import serial
 import os
+import sys
+import json
+
+SERIAL_DEV = "/dev/FTDI"
+SERIAL_SPEED = 115200
 
 def send_read_line(ser, cmd):
     ser.write(cmd + "\n")
@@ -16,7 +21,13 @@ def wait_on_string(ser, string):
                 return True
     return False
 
-def login(ser):
+def send_file(ser, local_file, remote_folder):
+    ser.write("cd %s\n" % remote_folder)
+    ser.write("rz -w 8192\n")
+    os.system("stty -F %s %s" % (SERIAL_DEV, SERIAL_SPEED))
+    os.system("sz -y -w 8192 %s > %s < %s" % (local_file, SERIAL_DEV, SERIAL_DEV))
+
+def login(ser, user, password):
     send_read_line(ser, "")
     if "#" in send_read_line(ser, ""):
         print "logged in"
@@ -24,26 +35,55 @@ def login(ser):
     print "logging in..."
     ser.write("\n")
     if wait_on_string(ser, "login:"):
-        ser.write("root\r\n")
+        ser.write("%s\r\n" % user)
         if wait_on_string(ser, "Password:"):
-            ser.write("Kubos123\r\n")
+            ser.write("%s\r\n" % password)
             return wait_on_string(ser, "#")
     return False
 
+
+def run_show_output(remote_file):
+    blank_lines = 0
+    line = ""
+
+    ser.write("%s\n" % remote_file)
+
+    while blank_lines < 3:
+        line = ser.readline().strip()
+        if line == "":
+            blank_lines = blank_lines + 1
+        else:
+            blank_lines = 0
+            print line
+
+password = ""
+dest_dir = ""
+init_boot = ""
+run_level = ""
+app_name = ""
+
+with open('yotta_config.json', 'r') as f:
+    j = json.load(f)
+    password = j.get("system").get("password").encode('ascii')
+    dest_dir = j.get("system").get("destDir").encode('ascii')
+    init_boot = j.get("system").get("initAtBoot")
+    run_level = j.get("system").get("runLevel")
+    init_flash = j.get("system").get("initAfterFlash")
+    app_name = j.get("name", "exec")
+
+app_name = "supervisor-client"
+
+init_scripts = "%s%s" % (run_level, app_name)
+
 ser = serial.Serial('/dev/FTDI', 115200, timeout=1)
 
-if login(ser):
+if login(ser, "root", password):
     print "Sending app"
-    ser.write("cd /home/system/usr/bin\n")
-    ser.write("rm -f supervisor-client\n")
-    ser.write("rz -w 8192\n")
-    os.system("stty -F /dev/FTDI 115200")
-    os.system("sz -y -w 8192 source/supervisor-client > /dev/FTDI < /dev/FTDI")
+    send_file(ser, "source/supervisor-client", dest_dir)
     ser.write("\n")
+    
     print "Running app"
-    ser.write("/home/system/usr/bin/supervisor-client\n")
-    for l in ser.readlines():
-        print l.strip()
+    run_show_output("%s/%s" % (dest_dir, app_name))
     ser.write("exit\n")
 else:
     print "Unable to login"
