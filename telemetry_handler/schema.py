7797a4232eab28a5afc2184eb01f70b001588962
Ryan Plauche <ryan@kubos.co>
2017-10-23 17:37:16 -0500

    Got filtering working on subsystem, param, and timestamp ranges.

--- telemetry_handler/schema.py
+++ telemetry_handler/schema.py
@@ -2,43 +2,34 @@ import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from models import db_session, Telemetry as TelemetryModel
-""""
-class Telemetry(SQLAlchemyObjectType):
-    class Meta:
-        model = TelemetryModel
-        interfaces = (relay.Node, )
-
-class TelemConnectionField(SQLAlchemyConnectionField):
-    RELAY_ARGS = ['first', 'last', 'before', 'after']
-
-    @classmethod
-    def get_query(cls, model, context, info, args):
-        query = super(TelemConnectionField, cls).get_query(model, context, info, args)
-        for field, value in args.items():
-            if field not in cls.RELAY_ARGS:
-                query = query.filter(getattr(model, field) == value)
-        return query
-
-class Query(graphene.ObjectType):
-    telemetery = TelemConnectionField(Telemetry,
-                                           param=graphene.String(),
-                                           subsystem=graphene.String(),
-                                           timestamp=graphene.Int(),
-                                           value=graphene.Int())
-
-
-schema = graphene.Schema(query=Query)
-"""
 
 class Telemetry(SQLAlchemyObjectType):
     class Meta:
         model = TelemetryModel
 
 class Query(graphene.ObjectType):
-    telemetry = graphene.List(Telemetry)
+    telemetry = graphene.List(Telemetry,
+                              subsystem=graphene.String(),
+                              param=graphene.String(),
+                              timestamp_before=graphene.Float(),
+                              timestamp_after=graphene.Float())
 
-    def resolve_telemetry(self, info, subsystem=None):
-        print("resolving telemetry")
-        return Telemetry.get_query(info).all()
+    def resolve_telemetry(self,
+                          info,
+                          subsystem=None,
+                          param=None,
+                          timestamp_before=None,
+                          timestamp_after=None):
+        model = Telemetry._meta.model
+        query = Telemetry.get_query(info)
+        if subsystem:
+            query = query.filter(model.subsystem == subsystem)
+        if param:
+            query = query.filter(model.param == param)
+        if timestamp_before:
+            query = query.filter(model.timestamp <= timestamp_before)
+        if timestamp_after:
+            query = query.filter(model.timestamp >= timestamp_after)
+        return query.all()
 
 schema = graphene.Schema(query=Query)
